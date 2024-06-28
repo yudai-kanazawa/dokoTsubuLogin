ab8071e17d0df0b490927b2164552e44bb4ae0ab
yudai-kanazawa <yudai-kanazawa@kccs.co.jp>
2024-06-28 17:30:46 +0900

    #7 add AccountsDAO

---
+++ dokoTsubuLogin/src/main/java/dao/AccountsDAO.java
@@ -0,0 +1,12 @@
+package dao;
+import model.User;
+import model
+
+
+public class AccountsDAO {
+	private final String JDBC_URL = "jdbc:h2:tcp://localhost/~/dokoTsubu";
+	private final String DB_USER = "sa";
+	private final String DB_PASS = "";
+	
+	public Account findByUserId(User user) {}
+}
